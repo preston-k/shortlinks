ce7d14cbd76dd7fa078d4d1b108fffc620eb2e0c
Preston Kwei <prestonkwei@gmail.com>
2024-11-26 20:40:26 -0800

    reorder

---
+++ script.js
@@ -0,0 +1,20 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAo5O10Cp_J1mRnjbzlO73Bh4CzON_M3DE',
+  authDomain: 'link-track-2a944.firebaseapp.com',
+  databaseURL: 'https://link-track-2a944-default-rtdb.firebaseio.com',
+  projectId: 'link-track-2a944',
+  storageBucket: 'link-track-2a944.firebasestorage.app',
+  messagingSenderId: '1067163047529',
+  appId: '1:1067163047529:web:42db51ec6e467d9f5974f8',
+}
+firebase.initializeApp(firebaseConfig)
+let database = firebase.database()
+
+const urlParams = new URLSearchParams(window.location.search)
+let pre = urlParams.get('pre')
+if (pre == null || pre == '' || pre == undefined) {
+  document.querySelector('#load').style.display = 'none'
+  document.querySelector('#error').style.display = 'flex'
+}
+
+console.log(pre)
